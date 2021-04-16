130f175159cf754e0ae40d8b18cce5f9f5a073cc
rjulio <auxdesarrollo@aceitesa.com>
2021-04-15 23:58:10 -0500

    feature initial single page and resources added

--- js/main.js
+++ js/main.js
@@ -1,20 +1,38 @@
 $(document).ready(function() {
 
-    $('[data-toggle="tooltip"]').tooltip();
-
-    $('.carousel').carousel({
-        interval: 6000,
-        keyboard: true,
-        pause: "hover"
-    });
+    // $('[data-toggle="tooltip"]').tooltip();
 
     $(document).on('click', '.nav-link', handlerClickNavItem);
-    $(document).on('click', '.btnTop', handlerClickNavItem);
 
 });
 
-function handlerClickNavItem(e) {
-    $('.nav-link').removeClass('active');
-    $(this).addClass('active');
-    const section = $(this).attr('href');
+function handlerClickNavItem() {
+    let contentSpace = $('#contentSpace');
+    const section = $(this).attr('templatehtml');
+
+    if (section.toLowerCase() === 'inicio') {
+        contentSpace.html('');
+        $('#carouselExampleIndicators').show();
+        return;
+    }
+
+    console.log('url', `http://127.0.0.1:5500/views/${section}.html`);
+
+    $.ajax({
+        type: 'GET',
+        url: `/views/${section.toLowerCase()}.html`,
+        beforeSend: function wait() {
+            contentSpace.html(`
+                <div class="fa-1x text-primary" id="loading">
+                    <!--<i class="fas fa-cog fa-spin"></i>--> Cargando...
+                </div>
+            `);
+        },
+        success: function(data) {
+            $('#carouselExampleIndicators').hide();
+            contentSpace.html(data);
+            $('.nav-link').removeClass('active');
+            $(this).addClass('active');
+        }
+    });
 }
