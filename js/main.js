c159d90df60fe38fb293a06988e851f37b6ceebb
rjulio <auxdesarrollo@aceitesa.com>
2021-04-28 19:10:21 -0500

    algunos cambios

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
 function handlerClickNavItem() {
     let contentSpace = $('#contentSpace');
     const section = $(this).attr('templatehtml');
+    let optionMenu = $(this);
 
     if (section.toLowerCase() === 'inicio') {
         contentSpace.html('');
@@ -16,23 +17,19 @@ function handlerClickNavItem() {
         return;
     }
 
-    console.log('url', `http://127.0.0.1:5500/views/${section}.html`);
-
     $.ajax({
         type: 'GET',
         url: `/views/${section.toLowerCase()}.html`,
-        beforeSend: function wait() {
-            contentSpace.html(`
-                <div class="fa-1x text-primary" id="loading">
-                    <!--<i class="fas fa-cog fa-spin"></i>--> Cargando...
-                </div>
-            `);
+        beforeSend: function() {
+            $('#loading').show();
         },
         success: function(data) {
             $('#carouselExampleIndicators').hide();
             contentSpace.html(data);
             $('.nav-link').removeClass('active');
-            $(this).addClass('active');
+            optionMenu.addClass('active');
+
+            $('#loading').hide();
         }
     });
 }
