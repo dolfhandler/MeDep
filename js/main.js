d6f8b9c6215d3df823dcf81224193ece47fe1be1
rjulio <auxdesarrollo@aceitesa.com>
2021-05-09 18:54:09 -0500

    feature interacion con el blog listo

--- js/main.js
+++ js/main.js
@@ -33,10 +33,12 @@ function handlerClickNavItem() {
         return;
     } else if (section.toLowerCase() === 'blog') {
         contentSpace.html('');
+        contentSpace.addClass("mt-4");
 
         for (let content of blog.content) {
-            contentSpace.append(`<section id="${content.id}" style="padding: 5px; border: 1px solid #009900;"></section>`)
-            builtBlogHTML(content, contentSpace);
+            // border: 1px solid #009900;
+            contentSpace.append(`<section id="${content.id}" style="padding: 5px;  width: 100%; display: flex; flex-direction: row;" ></section>`)
+            builtBlogHTML(content);
         }
 
 
@@ -70,8 +72,7 @@ async function writeBlogFile() {
     const result = await $.ajax({
         type: 'GET',
         url: `/js/blog/elementor-94-2021-05-07.json`,
-        contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
-        dataType: "json"
+        headers: { 'Access-Control-Allow-Origin': '*' }
     });
     return result;
 }
@@ -81,7 +82,7 @@ async function loadBlogFile() {
     console.log(blog);
 }
 
-function builtBlogHTML(_blog, container) {
+function builtBlogHTML(_blog) {
     const parentID = _blog.id;
     console.log('parent: ', parentID);
     // console.log('_blog: ', _blog);
@@ -92,48 +93,143 @@ function builtBlogHTML(_blog, container) {
         switch (content.elType) {
             case 'column':
                 console.log('colum: ', content);
-                drawColum(content, container, parentID);
+                drawColum(content, parentID);
+                break;
+            case 'section':
+                console.log('section: ', content);
+                drawColum(content, parentID);
                 break;
             case "widget":
                 console.log('widget: ', content);
-                drawWidget(content, container, parentID);
+                drawWidget(content, parentID);
                 break;
         }
 
-        // console.log('content.elements: ', content.elements);
-        // console.log('c: ', content.elements.lenght > 0);
         if (content.elType !== "widget") {
             console.log('sigue');
-            builtBlogHTML(content, container);
+            builtBlogHTML(content);
         }
     }
 
 }
 
-function drawColum(content, container, parentID) {
+function drawColum(content, parentID) {
     const parentElement = $(`#${parentID}`);
-    console.log('parentElement: ', $(parentElement));
+    console.log('contentColum: ', content);
 
-    $(parentElement).append(`<div id="${content.id}" style="padding: 5px; width: ${content.settings._column_size}%; border: 1px solid #900;"></div>`);
-    // container.append(parentElement);
+    // border: 1px solid #900;
+    $(parentElement).append(`<div id="${content.id}" style="padding: 5px; width: ${content.settings._column_size}%;"></div>`);
 }
 
-function drawWidget(content, container, parentID) {
+async function drawWidget(content, parentID) {
     const parentElement = $(`#${parentID}`);
-    console.log('parentElement: ', $(parentElement));
+    console.log('contentWidget: ', content);
 
     switch (content.widgetType) {
         case "text-editor":
-            console.log("text-editor: ", $(parentElement));
-            $(parentElement).append(`${content.settings.editor}`);
+            $(parentElement).append(`<div>${content.settings.editor}</div>`);
             break;
+        case "heading":
+            $(parentElement).append(`<div style="
+            text-align: ${content.settings.align};
+            color: ${content.settings.title_color};
+            font-family: ${content.settings.typography_font_family};
+            font-weight: ${content.settings.typography_font_weight};
+            font-size: ${content.settings.typography_font_size.size} ${content.settings.typography_font_size.unit};
+            text-align: ${content.settings.align};
+            ">
+            <h1>${content.settings.title}</h1>
+            </div>`);
+            break;
+        case "button":
+            console.log("button: ", $(parentElement));
+            $(parentElement).append(`
+            <div style="
+                text-align: ${content.settings.align};
+            ">
+            <a href="#" class="btn btn-primary"
+                style="
+                margin-right: ${content.settings.icon_indent.size}${content.settings.icon_indent.unit};
+                ">
+                ${content.settings.text}
+            </a>
+            </div>
+            `);
+            break;
+        case "image":
+            if (content.settings.image !== undefined)
+                $(parentElement).append(`<div style="text-align: center;">
+            <img src="${ChangeURL(content.settings.image.url)}" alt="" 
+            width="${content.settings.image_custom_dimension.width}"
+            height="${content.settings.image_custom_dimension.height}">
+            </div>`);
+            break;
+        case "image-box":
+            if (content.settings.image !== undefined) {
+                const url = ChangeURL(content.settings.image.url);
+                $(parentElement).append(`<div style="text-align: center;">
+                <img src="${url}" alt="" width="300" height="220">
+                <h1>${content.settings.title_text}</h1>
+                <p>${content.settings.description_text}</p>
+                </div>`);
+            }
+            break;
+        case "image-carousel":
+            let innerCarousel = '';
+            let flag = 0;
 
+            for (const image of content.settings.carousel) {
+                url = ChangeURL(image.url);
+
+                // <div  style="background: url('${url}') no-repeat center center/cover;  width:100%; height:800px; margin: auto;"></div>
+                innerCarousel += `
+                <div class="carousel-item ${flag===0?'active':''}">
+                <div class="d-block w-100">
+                <div  style="background: url('${url}') no-repeat top left / 100% 100%;  width:100%; height:650px; margin: auto;"></div>
+                </div>
+                </div>`;
+
+                if (flag === 0) {
+                    flag = 1;
+                }
+            }
+
+            $(parentElement).append(`<div style="text-align: center;">
+            
+            <div id="carouselBlog" class="carousel slide mb-4" data-ride="carousel">
+                <div class="carousel-inner">
+                    
+                        ${innerCarousel}
+                    
+                </div>
+                <a class="carousel-control-prev" href="#carouselBlog" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselBlog" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+
+            </div>`);
+            break;
+        case "star-rating":
+            let stars = '';
+            for (let i = 0; i < 5; i++) {
+                stars += `<i class="fas fa-star fa-1x"></i>`;
+            }
+            $(parentElement).append(`<div style="color: #FDCC0D">${stars}</div>`);
+            break;
         default:
             break;
     }
-    // container.append(parentElement);
+}
 
-    // container.append(`<div id="${content.id}" style="width: ${content.settings._column_size}%; border: 1px solid #900; height: 50px;"></div>`);
+function ChangeURL(url) {
+    const imagesBlog = "/image/blog";
+    const fileName = url.substring(url.lastIndexOf('/') + 1);
+    return `${imagesBlog}/${fileName}`;
 }
 
 function initializeTooltipServiceView() {
